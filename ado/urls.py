5c673586c0cc21f9860f7eeefa9121b8bc46aeb6
OJarvey <165787104+OJarvey@users.noreply.github.com>
2025-01-11 19:20:55 +0000

    Add social account templates and enhance user profile view

--- ado/urls.py
+++ ado/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
@@ -26,5 +27,5 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
-    path('profiles/', include('profiles.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
