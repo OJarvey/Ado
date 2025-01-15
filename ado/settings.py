ccf880a7080b92e10578bba08b5c8e6391b0f9c7
OJarvey <165787104+OJarvey@users.noreply.github.com>
2025-01-15 23:02:01 +0000

    Update settings for production security and environment configuration

--- ado/settings.py
+++ ado/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^h3js)h#r_zy%+9r@81j5)c+a5k#@tif)0^e+!)$rvjm&%5^+j'
+SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     'boutique-ado2025.herokuapp.com',
