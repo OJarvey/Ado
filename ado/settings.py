1cb2ad26357d43efefac3347366d4ea13aaf0b93
OJarvey <165787104+OJarvey@users.noreply.github.com>
2025-01-20 01:13:11 +0000

    update requirements

--- ado/settings.py
+++ ado/settings.py
@@ -202,8 +202,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'checkout/static'),  # Checkout static files
-    os.path.join(BASE_DIR, 'profiles/static'),  # Profiles static files
 ]
 
 
